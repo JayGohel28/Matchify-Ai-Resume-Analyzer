cd96c1f1dc0d67b8ccd23ffb12b8e536ac1295e3
JayGohel28 <jaygohel091@gmail.com>
2026-02-13 11:37:49 +0530

    Update HomeController.cs

--- MatchifyAi/Controllers/HomeController.cs
+++ MatchifyAi/Controllers/HomeController.cs
@@ -1,35 +1,18 @@
-using MatchifyAi.Models;
 using MatchifyAI.Data;
 using MatchifyAI.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace MatchifyAi.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        public IActionResult AdminMainForm()
-        {
-            return View();
-        }
-        public IActionResult RecruiterMainForm()
-        {
-            return View();
-        }
-        public IActionResult UserMainForm()
-        {
-            return View();
-        }
-        public IActionResult Login()
-        {
-            return View();
-        }
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -37,19 +20,6 @@ namespace MatchifyAi.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public HomeController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
         [HttpPost]
         public IActionResult Contact(ContactMessage model)
         {
@@ -63,5 +33,6 @@ namespace MatchifyAi.Controllers
             return View("Index");
         }
 
+        // other actions...
     }
 }
