1cbffde7caa28e764a036911bfc58211b5f658b5
JayGohel28 <jaygohel091@gmail.com>
2026-02-15 17:40:53 +0530

    Create Model

---
+++ MatchifyAi/Models/Job.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchifyAI.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string JobTitle { get; set; }
+
+        [Required]
+        public string ExperienceLevel { get; set; }
+
+        [Required]
+        public string RequiredSkills { get; set; }
+
+        [Required]
+        public string JobDescription { get; set; }
+
+        public string JobLocation { get; set; }
+
+        public string EmploymentType { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
